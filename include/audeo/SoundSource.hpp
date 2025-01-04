2bf0b8ac4ce9954dfa7637d63d95b2b98f1fcaec
lukas <tost>
2025-01-04 15:06:55 +0100

    fixed return statement

--- include/audeo/SoundSource.hpp
+++ include/audeo/SoundSource.hpp
@@ -3,6 +3,8 @@
 
 #include <SDL_mixer.h>
 #include <string_view>
+#include <inttypes.h>
+#include <stdint.h>
 
 #include "export_import.hpp"
 #include "exception.hpp"
@@ -15,13 +17,13 @@ enum class AudioType { Music, Effect };
 class SoundSource {
 public:
     SoundSource() : handle(-1) {}
-    SoundSource(std::int64_t handle) : handle(handle) {}
+    SoundSource(int64_t handle) : handle(handle) {}
     SoundSource(SoundSource const&) = default;
     SoundSource(SoundSource&&) = default;
 
     SoundSource& operator=(SoundSource const&) = default;
     SoundSource& operator=(SoundSource&&) = default;
-    std::int64_t value() const { return handle; }
+    int64_t value() const { return handle; }
 
     bool operator==(SoundSource const& rhs) const {
         return handle == rhs.handle;
@@ -31,7 +33,7 @@ public:
     }
 
 private:
-    std::int64_t handle;
+    int64_t handle;
 };
 
 } // namespace audeo
@@ -40,7 +42,7 @@ namespace std {
 template<>
 struct hash<audeo::SoundSource> {
     size_t operator()(audeo::SoundSource const& x) const {
-        return hash<std::int64_t>()(x.value());
+        return hash<int64_t>()(x.value());
     }
 };
 } // namespace std
